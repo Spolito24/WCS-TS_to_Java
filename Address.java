16b2302130571557caa3985c76f89e03795e141c
Spolito24 <baptisterenier69@gmail.com>
2023-09-27 14:53:43 +0200

    add constructor and getter and setter

--- Address.java
+++ Address.java
@@ -3,7 +3,47 @@ public class Address {
     private String street;
     private String city;
     private String country;
+
+    public Address(String title, String street, String city, String country) {
+        this.title = title;
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }    
 }
+
 
 // export class Address {
 //     number: string;
--- User.java
+++ User.java
@@ -6,6 +6,46 @@ public class User {
     private int size;
     private List<Photo> photos;
     private Address address;
+    
+
+    public User(Date birthDate, int size, List<Photo> photos, Address address) {
+        this.birthdate = birthDate;
+        this.size = size;
+        this.photos = photos;
+        this.address = address;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
 
 // import { Address } from './address';
